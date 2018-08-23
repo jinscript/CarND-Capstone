8ff5de34c1ca72cdd99153e72d6530b7eed76adf
Jin Sun <jins@uber.com>
2018-08-22 22:21:26 -0700

    fix

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -147,7 +147,7 @@ class TLDetector(object):
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
         if self.pose:
-            car_wp_idx = self.get_closest_waypoint(self.pose.pose.potition.x, self.pose.pose.potition.y)
+            car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
 
             # DONE: find the closest visible traffic light (if one exists)
             diff = len(self.waypoints.waypoints)
